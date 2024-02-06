34e8319028a60396374d788bb0861de05cc38b3e
mbrunetto <matteo.brunetto@lastminute.com>
2024-02-06 10:13:36 +0100

    first commit

--- app/src/main/kotlin/io/doubleloop/version0/Domain.kt
+++ app/src/main/kotlin/io/doubleloop/version0/Domain.kt
@@ -10,12 +10,35 @@ package io.doubleloop.version0
 // TODO 1: Those type alias are only placeholders,  use correct/better type definitions.
 //  Remember to be as strict as possible but also to keep it simple.
 
-typealias Rover = Nothing
-typealias Position = Nothing
-typealias Orientation = Nothing
+data class Rover(val position: Position, val orientation: Orientation)
+data class Position(val x: Int, val y: Int)
+sealed class Orientation {
+    data object North: Orientation()
+    data object South: Orientation()
+    data object West: Orientation()
+    data object East: Orientation()
+}
+enum class Orientation2 {
+    NORTH,
+    SOUTH,
+    WEST,
+    EST
+}
 
-typealias Planet = Nothing
-typealias Size = Nothing
-typealias Obstacle = Nothing
+data class Planet(val size: Size, val obstacles: List<Obstacle>)
+data class Size(val width: Int, val height: Int)
+data class Obstacle(val position: Position)
 
-typealias Command = Nothing
+sealed class Command {
+    data object Left: Command()
+    data object Right: Command()
+    data object Forward: Command()
+    data object Backward: Command()
+}
+
+enum class Command2 {
+    LEFT,
+    RIGHT,
+    FORWARD,
+    BACKWARD
+}
