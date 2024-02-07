d946ad47dc537d7d1664439da346c6ea5dd2d0ab
mbrunetto <matteo.brunetto@lastminute.com>
2024-02-07 12:57:26 +0100

    v4

--- app/src/main/kotlin/io/doubleloop/Main.kt
+++ app/src/main/kotlin/io/doubleloop/Main.kt
@@ -1,5 +1,12 @@
 package io.doubleloop
 
+import io.doubleloop.version4.runApp
+import kotlinx.coroutines.runBlocking
+
 fun main() {
-    println("[APP] Hello, world!")
+    runBlocking {
+            println("[APP] Hello, world!")
+            runApp("planet.txt", "rover.txt")
+            println("[APP] Done")
+    }
 }
